6deea6f197e6a5a3238d6be963810a42a5bfab2d
natyhtpsm <119613135+natyhtpsm@users.noreply.github.com>
2023-01-13 17:59:06 +0100

    Update randomserie.py

--- randomserie.py
+++ randomserie.py
@@ -1,12 +1,20 @@
 # Name: Nathália Pimentel de Assis
 
+# Version one: completely random choice, not based in genre
+# from random import choice
+# series = ['breaking bad', 'prison break', 'greys anatomy', 'how i met your mother', 'game of thrones'] ]
+# print (choice(series))
+
+
+#Version two below: 
+
 series = [['breaking bad', 'drama', ] 
          ['prison break', 'action']
          ['greys anatomy','medical'] 
          ['how i met your mother', 'comedy']
          ['game of thrones', 'adventure']]
-print('Choose the gender of your serie:  ')
-gender = input()
+print('Choose the genre of your serie:  ')
+genre = input()
 for item in series:
-  if item[1] == gender:
+  if item[1] == genre:
     print(gender + 'serie:' + item[0])
